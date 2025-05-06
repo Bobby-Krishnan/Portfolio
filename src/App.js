cc6d8a0e85e91bc9deb321765e0c248296131c56
Bobby-Krishnan <Abhishek.v.krishnan@gmail.com>
2025-05-06 07:33:36 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import React from "react";
 function App() {
   return (
     <div className="font-sans bg-gray-50 text-gray-800">
-      
+      {/* Navbar */}
+      <nav className="p-4 shadow bg-white sticky top-0 z-10">
+        <div className="container mx-auto flex justify-between items-center">
+          <h1 className="text-xl font-bold">Bobby Krishnan</h1>
+          <div className="space-x-4">
+            <a href="#about" className="hover:text-blue-500">About</a>
+            <a href="#projects" className="hover:text-blue-500">Projects</a>
+            <a href="#contact" className="hover:text-blue-500">Contact</a>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
